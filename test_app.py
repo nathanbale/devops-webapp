c1a626992b2b9f1574047cfa897b61636ce8b0a4
Nathan Bale <balenathan23@gmail.com>
2025-05-19 11:39:26 +0000

    simulate test failure

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_home():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
-    assert b"DevOps" in response.data  # Adjust to match your actual output
+    assert b"something wrong" in response.data
