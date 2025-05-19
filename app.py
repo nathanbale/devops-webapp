31fd53f82f9a5ee7597113596a6125a0ba2f590f
Nathan Bale <balenathan23@gmail.com>
2025-05-19 10:51:45 +0000

    Testing CI/CD pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, DevOps world!"
+    return "Hello, DevOps world! If you see this, your CI/CD works!"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
