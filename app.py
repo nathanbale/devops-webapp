c19057d0f3cce2cdcb95359d1bc6917716a0ebbb
Nathan Bale <balenathan23@gmail.com>
2025-05-21 12:46:57 +0000

    Test staging CI/CD pipeline

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return f"Hello, DevOps world! If you see this, your CI/CD works! Current time: {now}"
+    return f"Hello, staging environment! CI/CD is working! Current time: {now}"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
