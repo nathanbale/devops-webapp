3331ad1826900cd4fd4cc0f6c61464a9c7f5cc5a
Nathan Bale <balenathan23@gmail.com>
2025-05-30 08:41:27 +0000

    Test production CI/CD pipeline

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_home():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
-    assert b"DevOps world" in response.data  # <-- should pass
+    assert b"Hello, prduction" in response.data
