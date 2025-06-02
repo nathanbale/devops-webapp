43b7c2bb4b06471b1df759e3f711c575bb85dbf1
Nathan Bale <balenathan23@gmail.com>
2025-06-02 12:21:09 +0000

    test production changes

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_home():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
-    assert b"Hello,Nathan!" in response.data
+    assert b"Hello, Nathan!" in response.data
